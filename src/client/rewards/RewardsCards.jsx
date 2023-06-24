c93598ed37fdd3a28493d99e96673915a95a7471
rn-mgl <reinb2ra@gmail.com>
2023-06-24 16:48:40 +0800

    changed filter of not received reward to a question mark svg

--- src/client/rewards/RewardsCards.jsx
+++ src/client/rewards/RewardsCards.jsx
@@ -1,30 +1,28 @@
 import Image from "next/image";
 import React from "react";
 import Link from "next/link";
+import question from "../../../public/Question.svg";
 
 const RewardsCards = (props) => {
+  const rewardImage = props.isReceived ? props.image : question;
   return (
     <div className="bg-white p-5 rounded-2xl cstm-flex-col gap-4 w-fit shadow-solid">
       <div className="w-full h-fit cstm-flex-col overflow-clip bg-accntColor p-2 rounded-2xl">
         <Image
-          src={props.image}
+          src={rewardImage}
           alt="temp"
           draggable={false}
-          className={`${
-            props.isReceived
-              ? "saturate-100 brightness-100 contrast-100"
-              : "saturate-0 brightness-0 contrast-50"
-          } rounded-2xl drop-shadow-md`}
+          className="rounded-2xl drop-shadow-md"
           width={230}
           height={200}
         />
+
+        {!props.isReceived ? <p className="text-xs italic font-light">not yet obtained</p> : null}
       </div>
 
-      <div className="cstm-flex-row w-full">
-        <div className="cstm-flex-row gap-1 font-poppins w-full">
-          <p className="font-bold text-black mr-auto">{props.title}</p>
-          <p className="opacity-50 text-sm capitalize">{props.type}</p>
-        </div>
+      <div className="cstm-flex-row gap-1 font-poppins w-full">
+        <p className="font-bold text-black mr-auto">{props.title}</p>
+        <p className="opacity-50 text-sm capitalize">{props.type}</p>
       </div>
 
       <Link
