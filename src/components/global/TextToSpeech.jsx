4d76cfd9bc77b67b8692efec74deefc7515368eb
rn-mgl <reinb2ra@gmail.com>
2023-09-27 17:26:18 +0800

    workaround on voice

--- src/components/global/TextToSpeech.jsx
+++ src/components/global/TextToSpeech.jsx
@@ -45,12 +45,7 @@ const TextToSpeech = (props) => {
       steffan: voices[118],
     };
 
-    if (voices[primaryVoiceIndex]?.name === "Microsoft Michelle Online (Natural) - English (United States)") {
-      SSU.voice = voices[primaryVoiceIndex];
-    } else {
-      SSU.voice = voices[secondaryVoiceIndex || 0];
-    }
-
+    SSU.voice = voices[primaryVoiceIndex || secondaryVoiceIndex || 0];
     SSU.text = words;
     SSU.volume = volume.apply;
     SSU.rate = rate;
