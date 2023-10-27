f88580b091b63523d917f589899be406b3b79a6f
rn-mgl <reinb2ra@gmail.com>
2023-10-27 19:20:35 +0800

    made test result from student tabular or grid

--- src/client/tests/TestRecord.jsx
+++ src/client/tests/TestRecord.jsx
@@ -22,14 +22,22 @@ const TestRecord = (props) => {
     const isCorrect = q.choice === q.answer;
     return (
       <div
-        className="p-4 bg-white rounded-md w-full cstm-flex-col gap-4 items-start t:w-10/12 l-l:w-8/12 shadow-md"
+        className="p-4 bg-white rounded-2xl w-full h-full border-2 border-neutral-300 
+                  cstm-flex-col gap-4 justify-start shadow-solid"
         key={q.question_id}
       >
-        <p className="font-bold">{q.question}</p>
+        <div className="w-full h-44 overflow-y-auto cstm-scrollbar">
+          <p className="font-semibold">{q.question}</p>
+        </div>
+
         <div className="cstm-separator" />
-        <div className="cstm-flex-col gap-3 w-full t:cstm-flex-row t:flex-wrap">
-          <div className="w-full h-full cstm-flex-col gap-4 t:cstm-flex-row">
-            <div className="cstm-flex-col w-full gap-2 text-center t:w-full bg-accntColor rounded-md p-2 h-full">
+
+        <div className="cstm-flex-col gap-3 w-full h-full t:cstm-flex-row t:flex-wrap">
+          <div className="w-full h-full cstm-flex-col gap-4">
+            <div
+              className={`cstm-flex-col w-full gap-2 text-center t:w-full 
+                rounded-md p-2 h-full ${isCorrect ? "bg-green-100" : "bg-red-100"}`}
+            >
               <div className="cstm-flex-row gap-2  w-full ">
                 <p className="font-bold text-prmColor">Your Answer</p>
                 <div className={`${isCorrect ? "bg-prmColor" : "bg-scndColor"} rounded-full cstm-flex-col`}>
@@ -40,7 +48,7 @@ const TestRecord = (props) => {
               <p className={`${q.choice ? "opacity-100" : "opacity-50"}`}>{q.choice ? q.choice : "No Answer"}</p>
             </div>
 
-            <div className="cstm-separator t:hidden" />
+            <div className="cstm-separator" />
 
             <div className="cstm-flex-col w-full gap-2 text-center t:w-full bg-prmColor p-2 rounded-md h-full">
               <p className="font-bold text-scndColor">Answer</p>
@@ -75,16 +83,16 @@ const TestRecord = (props) => {
 
   return (
     <div
-      className="fixed w-full top-0 left-0 h-full backdrop-blur-md z-30 p-4 
+      className="fixed w-full top-0 left-0 h-full bg-accntColor z-30
                   cstm-flex-col justify-start overflow-y-auto cstm-scrollbar-2"
     >
       {message.active ? <Message setMessageStatus={setMessageStatus} /> : null}
-      <div className="w-full h-auto cstm-w-limit cstm-flex-col justify-start gap-4 ">
+      <div className="w-full h-auto cstm-w-limit cstm-flex-col justify-start gap-4 p-4">
         <button onClick={() => props.handleSeeTestRecord(props.testId)} className="cstm-bg-hover ml-auto">
           <IoClose className="text-prmColor scale-150" />
         </button>
 
-        {questionsAndAnswer}
+        <div className="w-full h-full grid grid-cols-1 t:grid-cols-2 l-l:grid-cols-3 gap-4">{questionsAndAnswer}</div>
       </div>
     </div>
   );
