fcee4fbe27ce278c470c8fccf439d388f4bac6ed
rn-mgl <reinb2ra@gmail.com>
2024-01-03 20:03:54 +0800

    added google analytics

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,7 @@ import Hero from "@/components/landing/Hero";
 import LandingNav from "@/components/landing/LandingNav";
 import Offers from "@/components/landing/Offers";
 import Purpose from "@/components/landing/Purpose";
+import Script from "next/script";
 
 export const metadata = {
   title: "Readefine",
@@ -13,6 +14,14 @@ export const metadata = {
 export default function Home() {
   return (
     <main className="w-full h-full flex flex-col items-center justify-start">
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-0WXLXZD691"></Script>
+      <Script id="google-analytics">
+        {`  window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        
+        gtag('config', 'G-0WXLXZD691');`}
+      </Script>
       <LandingNav />
       <Hero />
       <Definition />
